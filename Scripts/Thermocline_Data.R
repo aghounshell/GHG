1288adfe66cc141b8d6399744c100074b887fe8c
aghounshell <alexgh@vt.edu>
2019-06-13 09:35:35 -0400

    Updated code to merge CTD and YSI casts to maximize dates with profiles

--- Scripts/Thermocline_Data.R
+++ Scripts/Thermocline_Data.R
@@ -55,7 +55,59 @@ fcr_dates <- merge(ysi_fcr_date_list, ctd_fcr_date_list, by="dates", all.x=TRUE,
 names(ysi_fcr_date)[3] <- "Date"
 fcr_merge <- merge(ctd_fcr_date, ysi_fcr_date, by="Date", all.x=TRUE, all.y=TRUE)
 
-#### Need to think about how I want to merge the CTD and YSI data
-#### (and what data to select and if the merge function actually worked...)
+# Select certain columns
+fcr_merge <- fcr_merge %>% select(Date,Reservoir.x,Site.x,Depth_m.x,Temp_C.x,DO_mgL.x,Cond_uScm.x,
+                                  Chla_ugL,Turb_NTU,pH.x,ORP_mV.x,PAR_umolm2s.x,Reservoir.y,
+                                  Site.y,Depth_m.y,Temp_C.y,DO_mgL.y,Cond_uScm.y,PAR_umolm2s.y,
+                                  ORP_mV.y,pH.y)
+
+# Find where there are Na values in the CTD data: need to do it for each column
+ctd_fcr_na <- is.na(fcr_merge$Reservoir.x)
+fcr_merge$Reservoir.x[ctd_fcr_na] <- fcr_merge$Reservoir.y[ctd_fcr_na]
+
+ctd_fcr_na <- is.na(fcr_merge$Site.x)
+fcr_merge$Site.x[ctd_fcr_na] <- fcr_merge$Site.y[ctd_fcr_na]
+
+ctd_fcr_na <- is.na(fcr_merge$Depth_m.x)
+fcr_merge$Depth_m.x[ctd_fcr_na] <- fcr_merge$Depth_m.y[ctd_fcr_na]
+
+ctd_fcr_na <- is.na(fcr_merge$Temp_C.x)
+fcr_merge$Temp_C.x[ctd_fcr_na] <- fcr_merge$Temp_C.y[ctd_fcr_na]
+
+ctd_fcr_na <- is.na(fcr_merge$DO_mgL.x)
+fcr_merge$DO_mgL.x[ctd_fcr_na] <- fcr_merge$DO_mgL.y[ctd_fcr_na]
+
+ctd_fcr_na <- is.na(fcr_merge$Cond_uScm.x)
+fcr_merge$Cond_uScm.x[ctd_fcr_na] <- fcr_merge$Cond_uScm.y[ctd_fcr_na]
+
+ctd_fcr_na <- is.na(fcr_merge$pH.x)
+fcr_merge$pH.x[ctd_fcr_na] <- fcr_merge$pH.y[ctd_fcr_na]
+
+ctd_fcr_na <- is.na(fcr_merge$ORP_mV.x)
+fcr_merge$ORP_mV.x[ctd_fcr_na] <- fcr_merge$ORP_mV.y[ctd_fcr_na]
+
+ctd_fcr_na <- is.na(fcr_merge$PAR_umolm2s.x)
+fcr_merge$PAR_umolm2s.x[ctd_fcr_na] <- fcr_merge$PAR_umolm2s.y[ctd_fcr_na]
+
+fcr_all <- fcr_merge %>% select(Date,Reservoir.x,Site.x,Depth_m.x,Temp_C.x,DO_mgL.x,Cond_uScm.x,
+                                Chla_ugL,Turb_NTU,pH.x,ORP_mV.x,PAR_umolm2s.x)
+names(fcr_all)[2] <- "Reservoir"
+names(fcr_all)[3] <- "Site"
+names(fcr_all)[4] <- "Depth_m"
+names(fcr_all)[5] <- "Temp_C"
+names(fcr_all)[6] <- "DO_mgL"
+names(fcr_all)[7] <- "Cond_uScm"
+names(fcr_all)[10] <- "pH"
+names(fcr_all)[11] <- "ORP_mV"
+names(fcr_all)[12] <- "PAR_umolm2s"
+
+# Check the number of unique days captured by the merged data set
+fcr_date_list <- as.data.frame(unique(as.Date(fcr_all$Date)))
+
+# Export out fcr_all as .csv
+# Includes merged data from CTD and YSI casts on unique dates
+# For dates with both a CTD and YSI cast, CTD casts were selected
+write_csv(fcr_all, path = "C:/Users/ahounshell/OneDrive/VT/GHG/GHG/Data_Output/FCR_CTDysi_merge.csv")
+
 
 # Save Rfile as Thermocline_Data
