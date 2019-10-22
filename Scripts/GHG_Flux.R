ba25b097bc60158592da91fb1b48501501e0a467
aghounshell <ahounshell10@gmail.com>
2019-10-22 13:50:01 -0400

    Started working on GHG flux data - need to come back with updated atmospheric data (CO2 and CH4).

--- Scripts/GHG_Flux.R
+++ Scripts/GHG_Flux.R
@@ -1,5 +1,7 @@
 ### Script to calculate CO2 and CH4 flux from FCR and BVR from Summer 2015 to Summer 2017
 ### A Hounshell, 10 Jun 2019
+### Updated to include separate reps for 0.1 m; constrained to Summer 2016 and 2017
+### 21 Oct 2019
 
 # Load libraries
 library(LakeMetabolizer)
@@ -10,12 +12,26 @@ library(scales)
 # Set working directory: using only output data files
 # Wind data: calculated for FCR when met data is not available
 # GHG data for BVR and FCR separated
-setwd("C:/Users/ahounshell/OneDrive/VT/GHG/GHG_R/Data_Output")
+setwd("C:/Users/ahoun/Dropbox/VT_GHG/GHG_R/Data_Output")
 
 # Read in .csv for FCR data, BVR data, and wind data
-fcr <- read.csv("GHG_FCR_0.1m.csv",header=TRUE)
-bvr <- read.csv("GHG_BVR_0.1m.csv",header=TRUE)
+fcr <- read.csv("GHG_FCR_0.1_reps.csv",header=TRUE)
+fcr$datetime <- as.POSIXct(strptime(fcr$datetime, "%Y-%m-%d", tz="EST"))
+bvr <- read.csv("GHG_BVR_0.1_reps.csv",header=TRUE)
+bvr$datetime <- as.POSIXct(strptime(bvr$datetime, "%Y-%m-%d", tz="EST"))
 wind <- read.csv("Calc_FCR_Wind.csv",header=TRUE)
+wind$Date <- as.POSIXct(strptime(wind$Date, "%Y-%m-%d", tz="EST"))
+
+# Separate FCR and BVR GHG Data into Reps 1 and 2: calculate fluxes for each rep
+# then calculate avg and stdev from both reps
+fcr_1 <- fcr %>% filter(rep==1) %>% group_by(datetime) %>% summarize_all(funs(mean)) %>% 
+  arrange(datetime) %>% mutate(grouping="Rep1")
+fcr_2 <- fcr %>% filter(rep==2) %>% group_by(datetime) %>% summarize_all(funs(mean)) %>% 
+  arrange(datetime) %>% mutate(grouping="Rep2")
+bvr_1 <- bvr %>% filter(rep==1) %>% group_by(datetime) %>% summarize_all(funs(mean)) %>% 
+  arrange(datetime) %>% mutate(grouping="Rep1")
+bvr_2 <- bvr %>% filter(rep==2) %>% group_by(datetime) %>% summarize_all(funs(mean)) %>% 
+  arrange(datetime) %>% mutate(grouping="Rep2")
 
 # Calculate 'k' for GHG calculations using the Cole method
 k600 <- k.cole.base(wind$met_wind) # units in m/day
@@ -24,13 +40,21 @@ names(k600data)[1] <- "datetime"
 
 # Merge k600 data and GHG data for FCR
 # Then approximate GHG data in between sample dates (use linear interpolation)
-fcr_ghg <- merge(k600data, fcr, by="datetime", all.x=TRUE, all.y=TRUE)
-fcr_ghg$co2_umol_L <- na.approx(fcr_ghg$co2_umol_L,na.rm=FALSE)
-fcr_ghg$ch4_umol_L <- na.approx(fcr_ghg$ch4_umol_L,na.rm=FALSE)
+fcr_ghg_1 <- merge(k600data, fcr_1, by="datetime", all.x=TRUE, all.y=TRUE)
+fcr_ghg_1$co2_umol_L <- na.approx(fcr_ghg_1$co2_umol_L,na.rm=FALSE)
+fcr_ghg_1$ch4_umol_L <- na.approx(fcr_ghg_1$ch4_umol_L,na.rm=FALSE)
+
+fcr_ghg_2 <- merge(k600data, fcr_2, by="datetime", all.x=TRUE, all.y=TRUE)
+fcr_ghg_2$co2_umol_L <- na.approx(fcr_ghg_2$co2_umol_L,na.rm=FALSE)
+fcr_ghg_2$ch4_umol_L <- na.approx(fcr_ghg_2$ch4_umol_L,na.rm=FALSE)
 
 # Calculate GHG flux
-ch4flux<-rep(-99,length(fcr_ghg$datetime)) # assuming Catm = 1.8 ppm or 0.1125 umol
-co2flux<-rep(-99,length(fcr_ghg$datetime))
+ch4flux_1<-rep(-99,length(fcr_ghg_1$datetime)) # assuming Catm = 1.8 ppm or 0.1125 umol
+co2flux_1<-rep(-99,length(fcr_ghg_1$datetime))
+ch4flux_2<-rep(-99,length(fcr_ghg_2$datetime)) 
+co2flux_2<-rep(-99,length(fcr_ghg_2$datetime))
+
+###########STOPPED TO PLAY AROUND WITH NET CDF FILES FOR ATMOS CONCENTRATIONS
 
 # Need to figure out where to get CH4 and CO2 atmospheric values***
 # Aka: what are the subtracted values below??
